c5a1475a21c4b44fa7b01694939a3518ee52af0e
Jake Waitze <jake@waitze.net>
2017-02-23 01:09:22 -0500

    Added better floating point precision using the decimal Python module. Modified the output to be a bit more descriptive

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+from decimal import *
+getcontext().prec = 100
+
 # Gregory-Leibniz Series
 def pi_gls(o):
     q_pi = 0
     for i, n in enumerate(range(1, int(o*2), 2)):
-        if i % 2 == 0:
-            q_pi += 1/n
-        else:
-            q_pi -= 1/n
+        r = Decimal(1/n)
+        q_pi = q_pi+r if i % 2 == 0 else q_pi-r
     return q_pi*4
 
 # first arg = factorial number
@@ -27,22 +28,20 @@ def factorial_(*args):
 def pi_ns(o):
     pi = 3
     for i, n in enumerate(range(2, 2+o*2, 2)):
-        if i % 2 == 0:
-            pi += 4/factorial(n+2, 3)
-        else:
-            pi -= 4/factorial(n+2, 3)
+        r = Decimal(4/factorial(n+2, 3))
+        pi = pi+r if i % 2 == 0 else pi-r
     return pi
 
 # Binomial Expansion
 def e_be(*args):
     n = 10**12 if len(args) < 1 else args[0]
-    return (1+(1/n))**n
+    return (1+(1/Decimal(n)))**n
 
 # Newton's Series
 def e_ns(o):
-    return sum([1/factorial_(i) for i in range(o)])
+    return sum([Decimal(1/factorial_(i)) for i in range(o)])
 
-#print(pi_gls(10**6))
-#print(pi_ns(10**6))
-print(e_ns(10**3))
-#print(e_be())
+print('pi, Gregory-Leibniz Series:', pi_gls(10**6))
+print('pi, Nilakantha Series:', pi_ns(10**6))
+print('e, Newton\'s Series:', e_ns(10**3))
+print('e, Binomial Expansion:', e_be())
