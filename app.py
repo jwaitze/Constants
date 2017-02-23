6834c5189b5c73e8e0c7be12fd52b80ae7ae8f1f
Jake Waitze <jake@waitze.net>
2017-02-23 00:09:11 -0500

    Added Nilakantha Series and comments labeling the respective methods of approximations

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-def pi(p):
+# Gregory-Leibniz Series
+def pi_gls(p):
     q_pi = 0
     for i, n in enumerate(range(1, int(p*2), 2)):
         if i % 2 == 0:
@@ -7,4 +8,17 @@ def pi(p):
             q_pi -= 1/n
     return q_pi*4
 
-print(pi(10**6))
+# Nilakantha Series
+def pi_ns(p):
+    def f(x):
+        return x*(x+1)*(x+2)
+    pi = 3
+    for i, n in enumerate(range(2, 2+p*2, 2)):
+        if i % 2 == 0:
+            pi += 4/f(n)
+        else:
+            pi -= 4/f(n)
+    return pi
+
+print(pi_gls(10**6))
+print(pi_ns(10**6))
