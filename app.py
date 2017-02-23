3606713f75eee3f9dd7aef10cb666006f104bf03
Jake Waitze <jake@waitze.net>
2017-02-23 00:38:50 -0500

    Added Newton's Series approximation for e and fixed an error in the factorialization function. Should modify the factorial function to not recurse, so that runs with higher iterations can occur

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Gregory-Leibniz Series
-def pi_gls(p):
+def pi_gls(o):
     q_pi = 0
-    for i, n in enumerate(range(1, int(p*2), 2)):
+    for i, n in enumerate(range(1, int(o*2), 2)):
         if i % 2 == 0:
             q_pi += 1/n
         else:
@@ -11,18 +11,23 @@ def pi_gls(p):
 # first arg = factorial number
 # optional second arg = amount of numbers to include in the factorial
 def factorial(*args):
-    n, i = args[0], 1 if len(args) != 2 else args[1]
+    n, i = args[0], args[0] if len(args) != 2 else args[1]
     return 1 if n < 1 or i < 1 else n*factorial(n-1, i-1)
 
 # Nilakantha Series
-def pi_ns(p):
+def pi_ns(o):
     pi = 3
-    for i, n in enumerate(range(2, 2+p*2, 2)):
+    for i, n in enumerate(range(2, 2+o*2, 2)):
         if i % 2 == 0:
             pi += 4/factorial(n+2, 3)
         else:
             pi -= 4/factorial(n+2, 3)
     return pi
 
-print(pi_gls(10**6))
-print(pi_ns(10**6))
+# Newton's Series
+def e_ns(o):
+    return sum([1/factorial(i) for i in range(o)])
+
+#print(pi_gls(10**6))
+#print(pi_ns(10**6))
+print(e_ns(100))
